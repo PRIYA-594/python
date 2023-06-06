dc957ec1c893cf056d53d010d61ac3f60b44b425
PRIYA-594 <97585592+PRIYA-594@users.noreply.github.com>
2023-06-06 16:20:27 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,17 @@
+list2=[12,14,-95,3]
+for i in range(0,len(list2)):
+    if(list2[i]>=0):
+        print(list2[i])
+#output
+#12
+#5
+#64
+
+list2=[12,14,-95,3]
+for i in range(0,len(list2)):
+    if(list2[i]>=0):
+        print(list2[i])
+#output
+#12
+#14
+#3
